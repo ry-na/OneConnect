1ef6d01c2dedc14572d986c443fe58aa3d5fc5ae
t-oot <taka.oot@gmail.com>
2018-03-03 00:28:45 +0900

    きれいに

--- web/app/Models/Session.php
+++ web/app/Models/Session.php
@@ -1,15 +1,11 @@
 <?php
-
 namespace App\Models;
-
 use App\Models\User;
 /**
  * @author Takahiro Otani <taka.oot@gmail.com>
  */
-class Session extends BaseModel
-{
+class Session extends BaseModel {
     const TABLE = 'sessions';
-
     /**
      * OPINION_ID
      */
@@ -22,62 +18,50 @@ class Session extends BaseModel
      * IP
      */
     const IP = 'ip';
-      /**
+    /**
      * TIMEOUT
      */
     const TIMEOUT = 360000;
-   /**
+    /**
      * RESULT
      */
-    const AUTH_SUCCESS=0x0000;
-    const AUTH_ERROR_TIMEOUT=0x1000;
-    const AUTH_ERROR_NOTFOUND=0x1001;
-      const AUTH_ERROR_INVALIDIP=0x1002;
+    const AUTH_SUCCESS = 0x0000;
+    const AUTH_ERROR_TIMEOUT = 0x1000;
+    const AUTH_ERROR_NOTFOUND = 0x1001;
+    const AUTH_ERROR_INVALIDIP = 0x1002;
     /**
      * @see https://readouble.com/laravel/5.3/ja/eloquent.html
      */
-    protected $guarded = [
-        self::ID
-    ];
-
+    protected $guarded = [self::ID];
     /**
      * Column to be obtained by Select statement.
      * @var array
      */
-    public static $gettableColumns = [
-        self::SESSION_ID,
-        self::USER_ID,
-        self::IP
-    ];
-public static function ErrorCode_Public($error){
-return ($error >> 4) << 4;
-}
-public static function SID2ID($request){
-  $sid = $request->header(static::SESSION_ID);
-$col =Session::where(static::SESSION_ID, $sid)->get()->first();
-if(!$col)return "";
-return $col->{Session::USER_ID};
-}
-public static function Auth($request) {
-  $sid = $request->header(static::SESSION_ID);
- $col = Session::where(static::SESSION_ID, $sid)->get()->first();
-if(!$col)return static::AUTH_ERROR_NOTFOUND;
-//CHECK1:TIME
-$time=$col->{Session::CREATED_AT}->timestamp;
- if(time()-$time>static::TIMEOUT)return static::AUTH_ERROR_TIMEOUT;
-//CHECK2:IP
-$ip=$col->{Session::IP};
-if($request->ip()!=$ip)return static::AUTH_ERROR_INVALIDIP;
-return static::AUTH_SUCCESS;
-}
-       public static function register($user,$ip)
-    {
-           $sid=bin2hex(random_bytes(32)); //256bit
-        static::create([
-            static::SESSION_ID => $sid,
-            static::USER_ID => $user->{User::EMAIL},
-            static::IP => $ip
-        ])->save();
-           return $sid;
+    public static $gettableColumns = [self::SESSION_ID, self::USER_ID, self::IP];
+    public static function ErrorCode_Public($error) {
+        return ($error >> 4) << 4;
+    }
+    public static function SID2ID($request) {
+        $sid = $request->header(static ::SESSION_ID);
+        $col = Session::where(static ::SESSION_ID, $sid)->get()->first();
+        if (!$col) return "";
+        return $col->{Session::USER_ID};
+    }
+    public static function Auth($request) {
+        $sid = $request->header(static ::SESSION_ID);
+        $col = Session::where(static ::SESSION_ID, $sid)->get()->first();
+        if (!$col) return static ::AUTH_ERROR_NOTFOUND;
+        //CHECK1:TIME
+        $time = $col->{Session::CREATED_AT}->timestamp;
+        if (time() - $time > static ::TIMEOUT) return static ::AUTH_ERROR_TIMEOUT;
+        //CHECK2:IP
+        $ip = $col->{Session::IP};
+        if ($request->ip() != $ip) return static ::AUTH_ERROR_INVALIDIP;
+        return static ::AUTH_SUCCESS;
+    }
+    public static function register($user, $ip) {
+        $sid = bin2hex(random_bytes(32)); //256bit
+        static ::create([static ::SESSION_ID => $sid, static ::USER_ID => $user->{User::EMAIL}, static ::IP => $ip])->save();
+        return $sid;
     }
 }
